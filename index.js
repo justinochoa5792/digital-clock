2282aea8828612c23727ca7fc3d9e926d8cb029d
Justin Ochoa <just.och92@gmail.com>
2023-06-04 10:48:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let hours = document.getElementById("hrs");
+let minutes = document.getElementById("min");
+let seconds = document.getElementById("sec");
+
+setInterval(() => {
+  let currentTime = new Date();
+
+  hours.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  minutes.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  seconds.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
